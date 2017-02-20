a916e6a24df566f4de8f7ce4fc1c7444453b5b36
Haruki Fukui <haruki.fukui@naviapis.com>
2017-02-21 06:42:11 +0900

    update commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const spawn = require('child_process').spawn;
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('./webpack.config');
+
+const devServerConfig = webpackConfig.devServer;
+const port = devServerConfig.port;
+const host = devServerConfig.host;
+
+new WebpackDevServer(webpack(webpackConfig), Object.assign(devServerConfig, {
+  publicPath: webpackConfig.output.publicPath,
+  stats: { colors: true }
+})).listen(port, host, err => {
+  if (err) {
+    return console.log(err);
+  }
+
+  spawn('nw', [], { stdio: 'inherit' })
+    .on('close', code => process.exit(code))
+    .on('error', err => console.error(err));
+
+  console.log(`Listening at ${host}:${port}`);
+});
